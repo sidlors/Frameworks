c9010a848b5e600a384a6472b7bfd6744f33929d
sidlors <sidlors@gmail.com>
2014-10-27 20:58:16 -0600

    Copia de paquetes

---
+++ hibernate/3_hibernate-jpa_1_d/src/main/java/com/example/as/AccionBursatilAS.java
@@ -0,0 +1,63 @@
+package com.example.as;
+
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+
+import com.example.dao.AccionBursatilDAO;
+//import com.example.entity.AccionBursatil;
+import com.example.entity.helper.AccionBursatilEH;
+import com.example.util.JPAUtil;
+
+public class AccionBursatilAS {
+
+	public void m1() throws Exception {
+//		EntityManager em = null;
+//		try {
+//			em = JPAUtil.getEMF1().createEntityManager();
+//
+//			em.getTransaction().begin();
+//
+//			AccionBursatilDAO accionBursatilDAO = new AccionBursatilDAO();
+//			accionBursatilDAO.setEntityManager(em);
+//
+//			AccionBursatil accionBursatil = new AccionBursatil();
+//			accionBursatil.setCodigoAccion(UUID.randomUUID().toString()
+//					.substring(0, 15));
+//			accionBursatil.setNombreAccion("Alimento para gatos");
+//			System.out.println(">>>accionBursatil:" + AccionBursatilEH.toString(accionBursatil) );
+//			accionBursatilDAO.persist(accionBursatil);
+//			// int i= 1/0; //error
+//
+//			AccionBursatil accionBursatil2 = new AccionBursatil();
+//			accionBursatil2.setCodigoAccion(UUID.randomUUID().toString()
+//					.substring(0, 15));
+//			accionBursatil2.setNombreAccion("Alimento para perros");
+//			System.out
+//					.println(">>>accionBursatil2:" +  AccionBursatilEH.toString(accionBursatil2) );
+//			accionBursatilDAO.persist(accionBursatil2);
+//
+//			em.getTransaction().commit();
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//
+//			try {
+//				if (em != null && em.getTransaction() != null
+//						&& em.getTransaction().isActive()) {
+//					em.getTransaction().rollback();
+//				}
+//			} catch (Exception e2) {
+//				e2.printStackTrace();
+//				throw e2;
+//			}
+//
+//			throw e;
+//		} finally {
+//			if (em != null) {
+//				em.close();
+//			}
+//		}
+	}
+
+}
